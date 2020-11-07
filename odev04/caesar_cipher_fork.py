27d5f1af483c6292dac974409daab513eb8febf5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-07 13:53:33 +0300

    fork changes, comments

--- odev04/caesar_cipher_fork.py
+++ odev04/caesar_cipher_fork.py
@@ -1,72 +1,95 @@
 import sys
+import time
 from multiprocessing import Lock, Process, Queue, current_process
 
 
 
-def encode_split (wq, rq, dict):
-    for data in iter(wq.get, 'STOP'):
-        print("%s encoding string block." % (current_process().name))
-        s = list(data)
-        i = 0
-        while i < len(s):
-            if s[i] in dict:
-                s[i] = dict[s[i]]
-            i += 1
-        data = "".join(s)
-        rq.put(data)
-        
+def encode_split (wq, rq, dict, qLock): #process function
+    while True:
+        qLock.acquire()       
+        if not wq.empty(): #is queue empty
+            data = wq.get() #get data
+            #qLock.release()
+            print("%s encoding string block." % (current_process().name))
+            s = list(data) #turn block into char list for easier operation 
+            i = 0
+            while i < len(s): #scan letters and switch non-special characters with their codes in the dictionary
+                if s[i] in dict:
+                    s[i] = dict[s[i]]
+                i += 1
+            data = "".join(s) #remake the block
+            #qLock.acquire()
+            rq.put(data) #send it in
+            qLock.release()
+        else:
+            qLock.release()
+            break
 
 
-def main():
-    s = int(sys.argv[1])
-    n = int(sys.argv[2])
-    l = int(sys.argv[3])
+def main(): #main process
+    s = int(sys.argv[1]) #shift
+    n = int(sys.argv[2]) #process count
+    l = int(sys.argv[3]) #process block size
 
     i = 97
     dict = {}
-    while (i < 123):
-        if (i + s < 123):
+    while (i < 123): #scan lowcase ascii letters
+        if (i + s < 123): #does shift exceed z? no, add to dictionary
             dict[chr(i)] = chr(i + s)
-        else:
+        else: #exceeds z, add leftover from the start of the alphabet
             dif = i + s - 122
             dict[chr(i)] = chr(96+dif)
         i+=1
 
     print("Printing encoded dictionary:\n", dict)
 
-    with open('input.txt', 'r') as file:
+    with open('input.txt', 'r') as file: #read input
         data = file.read()
     file.close()
 
-    data = data.lower()
-    split = [data[i:i+l] for i in range(0, len(data), l)]
-    workQueue = Queue()
+    data = data.lower() #input lower case
+    split = [data[i:i+l] for i in range(0, len(data), l)] #input divided into blocks by block size
+    workQueue = Queue() #queues up
     receiveQueue = Queue()
+    qLock = Lock()
     processes = []
 
-    for string in split:
+    for string in split: #put blocks into queue
         workQueue.put(string)
         
 
-    for w in range(n):
-        p = Process(target=encode_split, args=(workQueue, receiveQueue, dict))
+    for w in range(n): #create n processes and append them into the processes list
+        p = Process(target=encode_split, args=(workQueue, receiveQueue, dict, qLock))
         p.start()
         processes.append(p)
-        workQueue.put('STOP')
    
-    for p in processes:
-        p.join()
-    receiveQueue.put('STOP')
+    while not workQueue.empty(): #wait for work to be done
+        pass
+
+    time.sleep(2) #let last touches be done
+    k = 1
 
     encoded_split = [""]
-    for snippet in iter(receiveQueue.get, 'STOP'):
+    while not receiveQueue.empty(): #get encoded blocks (doesn't work properly after terminating processes)
         encoded_split.append(receiveQueue.get())
-
-    fName = "crypted_fork_{}_{}_{}.txt".format(str(s), str(n), str(l))
+    for p in processes: #work is done, terminate and join every process
+        if p.is_alive():
+            while p.is_alive(): #keep trying
+                p.terminate()
+                p.join()
+            print('process terminated .'+ str(k))
+            k += 1
+        else: #process is terminated
+            print('process terminated .'+ str(k))
+            k += 1
+    
+    
+    
+    fName = "crypted_fork_{}_{}_{}.txt".format(str(s), str(n), str(l)) #create new text file with parameters in the file name
     f = open(fName, "w")
-    for string in encoded_split:
+    for string in encoded_split: #write the blocks into the file
         f.write(string)
 
-if __name__ == '__main__':
+if __name__ == '__main__': #run main process
     main()
 
--- odev04/caesar_cipher_thread.py
+++ odev04/caesar_cipher_thread.py
@@ -5,69 +5,69 @@ import queue
 from multiprocessing import Queue
 
 
-s = int(sys.argv[1])
-n = int(sys.argv[2])
-l = int(sys.argv[3])
+s = int(sys.argv[1]) #shift
+n = int(sys.argv[2]) #thread count
+l = int(sys.argv[3]) #thread block size
 
 i = 97
 dict = {}
-while (i < 123):
-    if (i + s < 123):
+while (i < 123): #scan lowcase ascii letters
+    if (i + s < 123): #does shift exceed z? no, add to dictionary
         dict[chr(i)] = chr(i + s)
-    else:
+    else: #exceeds z, add leftover from the start of the alphabet
         dif = i + s - 122
         dict[chr(i)] = chr(96+dif)
     i+=1
 
 print("Printing encoded dictionary:\n", dict)
 
-with open('input.txt', 'r') as file:
+with open('input.txt', 'r') as file: #read input
     data = file.read()
 file.close()
 
-data = data.lower()
-split = [data[i:i+l] for i in range(0, len(data), l)]
+data = data.lower() #input lower case
+split = [data[i:i+l] for i in range(0, len(data), l)] #input divided into blocks by block size
 
-def encode_split (threadName, wq, rq, dict):
+def encode_split (threadName, wq, rq, dict): #thread called function
     while True:
         queueLock.acquire()
-        if not wq.empty():
+        if not wq.empty(): #if queue is not empty get data block
             data = wq.get()
             queueLock.release()
-            if data == "STOP":
+            if data == "STOP": #time to stop
                 print("%s received STOP request." % (threadName))
                 break
             print("%s encoding string block." % (threadName))
-            s = list(data)
+            s = list(data) #turn block into char list for easier operation
             i = 0
-            while i < len(s):
+            while i < len(s): #scan letters and switch non-special characters with their codes in the dictionary
                 if s[i] in dict:
                     s[i] = dict[s[i]]
                 i += 1
-            data = "".join(s)
+            data = "".join(s) #remake the block
             queueLock.acquire()
-            rq.put(data)
+            rq.put(data) #send it out
             queueLock.release()
         else:
             queueLock.release()
-        time.sleep(0.1)
+        time.sleep(0.1) #take a breather, keep the order
 
-class nThread (threading.Thread):
-    def __init__(self, threadID, name, dict, wq, rq):
+class nThread (threading.Thread): #thread class
+    def __init__(self, threadID, name, dict, wq, rq): #initiate name, dictionary and queues
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.dict = dict
         self.wq = wq
         self.rq = rq
-    def run(self):
+    def run(self): #call encode function when thread runs
         print("Starting", self.name)
         encode_split(self.name, self.wq, self.rq, self.dict)
         print("Stopping", self.name)
 
-threadList = ["Thread-1"]
+threadList = ["Thread-1"] #init threadList
 i = 0
-while (i < n):
+while (i < n): #create thread names by thread count
     threadList.append("Thread-%s" % str(i+1))
     i+=1
 queueLock = threading.Lock()
@@ -77,11 +77,11 @@ threads = []
 threadID = 1
 
 queueLock.acquire()
-for string in split:
+for string in split: #put blocks into queue
     workQueue.put(string)
 queueLock.release()
 
-for tName in threadList:
+for tName in threadList: #create threads, start them, add to threads list
     thread = nThread(threadID, tName, dict, workQueue, receiveQueue)
     thread.start()
     threads.append(thread)
@@ -89,22 +89,22 @@ for tName in threadList:
 
 
 
-while not workQueue.empty():
+while not workQueue.empty(): #waiting for work to be done
     pass
 
-for tName in threadList:
+for tName in threadList: #work is over, stop working
     workQueue.put("STOP")
 
-for t in threads:
+for t in threads: #wait for threads to stop
     t.join()
 
 encoded_split = [""]
-while not receiveQueue.empty():
+while not receiveQueue.empty(): #receive encoded blocks
     encoded_split.append(receiveQueue.get())
 
-fName = "crypted_thread_{}_{}_{}.txt".format(str(s), str(n), str(l))
+fName = "crypted_thread_{}_{}_{}.txt".format(str(s), str(n), str(l)) #open new text file with parameters included in filename
 f = open(fName, "w")
-for string in encoded_split:
+for string in encoded_split: #write the blocks into the file
     f.write(string)
 
 
